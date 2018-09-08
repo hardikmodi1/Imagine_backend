841664ff5c0f8462271ce55fc695954885ab0674
hardikmodi1 <hmodi2457@gmail.com>
2018-09-08 06:27:37 +0530

    boilerplate completed

---
+++ modules/forgotPassword/errorMessages.js
@@ -0,0 +1,2 @@
+export const expiredKeyError="Link is no longer valid";
+export const userNotFoundError="Could not find user with that email";
--- modules/login/errorMessages.js
+++ modules/login/errorMessages.js
@@ -2,3 +2,4 @@ export const invalidLogin="Invalid Credentials Provided!";
 export const errorFinding="Error in loggin in please try after some time!";
 export const invalidPassword="Password is incorrect!";
 export const emailNotVerified="Email is not verified!";
+export const forgotPassword="You have requested for password reset";
